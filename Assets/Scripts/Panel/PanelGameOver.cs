931fe32b12197e9316d17aea06509efbf3e7d91b
PhucDev02 <phuc12610@gmail.com>
2024-09-25 08:54:08 +0700

    Review
    -Layer wall,player còn đang lộn xộn, UI Pause layer còn thấp hơn cả wall
    -Em đang không làm theo concept của PanelManager,Panel nào cũng đang xếp sẵn ra scene, lại còn dùng ShadePanel(hơi dư thừa)
    -Đặt tên script của Panel chưa đúng convention
    -Event OnChangeHighscore chưa hợp lý
    -Chưa đổi tên string Playerpref của Highscore, em đang để là "" đấy
    -Bấm nút pause vẫn nhận input nhảy
    -Tap liên tục thì con chim bay được luôn
    .... Tạm thời thế đã nha

--- Assets/Scripts/Panel/PanelGameOver.cs
+++ Assets/Scripts/Panel/PanelGameOver.cs
@@ -1,9 +1,9 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class PanelGameOver : Panel
+public class PanelGameOver : Panel // mỗi panel lại đặt tên cấu trúc từ khác nhau vậy em, đặt theo tiếng anh thì mình để "Panel" ở cuối 
 { 
     public void OnClickRestartBtn()
     {
